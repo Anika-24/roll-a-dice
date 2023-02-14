39a11e62c405c088e4d48b44ac42aab90d5ce144
Anika-24 <76102260+Anika-24@users.noreply.github.com>
2023-02-14 11:11:07 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,9 @@
+roll-a-dice.py
+
+import random
+
+response=input("press y to roll again and n to exit")
+if(response==y):
+    no = random.randint(1,6)
+    #I couldnt complete this project as I dont know how to show the dice as hints are not showing.
+    
